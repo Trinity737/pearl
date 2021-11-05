7932554cc091d3257e03546f088192a3be1787d2
Trinity737 <85462888+Trinity737@users.noreply.github.com>
2021-11-05 11:11:39 -0400

    added Github repo Url

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by Cheavon Pottinger and is{" "}
-        <a href="">open-sourced on Github</a>
-      </footer>
+    <div className="container">
+      <div className="App">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by Cheavon Pottinger and is{" "}
+          <a href="https://github.com/Trinity737/pearl" target="_blank">
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
